d459c06aeffcc81d9ebb3737b6af78c78f77e659
Andres Ruiz Acevedo <andresruizaceved@gmail.com>
2023-05-08 16:18:49 +1000

    Adding working code for bookspider v3

--- projects/bookscraper/bookscraper/settings.py
+++ projects/bookscraper/bookscraper/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "bookscraper.pipelines.BookscraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "bookscraper.pipelines.BookscraperPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,6 +88,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
+# REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+# TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+# FEED_EXPORT_ENCODING = "utf-8"
--- projects/bookscraper/bookscraper/spiders/bookspider.py
+++ projects/bookscraper/bookscraper/spiders/bookspider.py
@@ -1,33 +1,22 @@
 import scrapy
+from bookscraper.items import BookItem
 
 
 class BookspiderSpider(scrapy.Spider):
-    
-    name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["http://books.toscrape.com/"]
-
+    name = 'bookspider'
+    allowed_domains = ['books.toscrape.com']
+    start_urls = ['https://books.toscrape.com/']
 
     def parse(self, response):
         books = response.css('article.product_pod')
-
-        # --- PART 4 ---
-        # for book in books:
-        #     yield {
-        #         'name': book.css('h3 a::text').get(),
-        #         'price': book.css('.product_price .price_color::text').get(),
-        #         'url': book.css('h3 a').attrib['href']
-        #     }
-        
-        # --- PART 5 ---
         for book in books:
-            relative_url = book.css('h3 a').attrib['href']
+            relative_url = book.css('h3 a ::attr(href)').get()
+
             if 'catalogue/' in relative_url:
                 book_url = 'https://books.toscrape.com/' + relative_url
             else:
                 book_url = 'https://books.toscrape.com/catalogue/' + relative_url
             yield response.follow(book_url, callback=self.parse_book_page)
-
 
         next_page = response.css('li.next a ::attr(href)').get()
         if next_page is not None:
@@ -40,19 +29,24 @@ class BookspiderSpider(scrapy.Spider):
 
     def parse_book_page(self, response):
 
-        table_rows = response.css('table tr')
-        yield {
-            'url': response.url,
-            'title': response.css('.product_main h1::text').get(),
-            'product_type': table_rows[1].css('td ::text').get(),
-            'price_excl_tax': table_rows[2].css('td ::text').get(),
-            'price_incl_tax': table_rows[3].css('td ::text').get(),
-            'tax': table_rows[4].css('td ::text').get(),
-            'availability': table_rows[5].css('td ::text').get(),
-            'num_reviews': table_rows[6].css('td ::text').get(),
-            'stars': response.css('p.star-rating').attrib['class'],
-            'category': response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-            'description': response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
-            'price': response.css('p.price_color ::text').get()
-        }
+        table_rows = response.css("table tr")
+        book_item = BookItem()
+
+    
+        book_item['url'] = response.url,
+        book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css("td ::text").get()
+        book_item['product_type' ] = table_rows[1].css("td ::text").get(),
+        book_item['price_excl_tax'] = table_rows[2].css("td ::text").get(),
+        book_item['price_incl_tax'] = table_rows[3].css("td ::text").get(),
+        book_item['tax'] = table_rows[4].css("td ::text").get(),
+        book_item['availability'] = table_rows[5].css("td ::text").get(),
+        book_item['num_reviews']=  table_rows[6].css("td ::text").get(),
+        book_item['stars'] = response.css("p.star-rating").attrib['class'],
+        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
+        book_item['description'] = response.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        book_item['price'] = response.css('p.price_color ::text').get(),
+    
+        yield book_item
+
 
