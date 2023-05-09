3df8ec0da26ec5a0dfd668746c23c527e6d2356f
Andres Ruiz Acevedo <andresruizaceved@gmail.com>
2023-05-09 13:12:09 +1000

    Set custom settings for output data

--- projects/bookscraper/bookscraper/settings.py
+++ projects/bookscraper/bookscraper/settings.py
@@ -13,9 +13,9 @@ SPIDER_MODULES = ["bookscraper.spiders"]
 NEWSPIDER_MODULE = "bookscraper.spiders"
 
 # Set general settings for output files
-FEEDS = {
-    'bookdata.json': {'format': 'json'}
-}
+# FEEDS = {
+#     'bookdata.json': {'format': 'json'}
+# }
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "bookscraper (+http://www.yourdomain.com)"
